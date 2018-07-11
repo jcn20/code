8d4015a654454b841321a7116464e8cf68316af2
jcn20 <julionunez96@gmail.com>
2018-07-11 14:07:48 -0400

    Changes made.

--- scripts/mapIt.py
+++ scripts/mapIt.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
 
 else: 
-    #Get address from clipboard.
+    # Get address from clipboard.
     address = pyperclip.paste()
 
 webbrowser.open('https://www.google.com/maps/place/' + address)
